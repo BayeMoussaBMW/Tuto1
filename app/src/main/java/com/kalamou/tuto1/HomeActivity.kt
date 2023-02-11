27f6fe25fc4b3a21e36ece2737002c5ad75044d3
Moussa <bayemoussawade@hotmail.fr>
2023-02-11 23:11:59 +0100

    Handle click from HomeActivity

--- app/src/main/java/com/kalamou/tuto1/HomeActivity.kt
+++ app/src/main/java/com/kalamou/tuto1/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.kalamou.tuto1
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -17,16 +18,18 @@ class HomeActivity: AppCompatActivity() {
         val icon = findViewById(R.id.ic1) as? ImageView
         icon?.setOnClickListener {
             Toast.makeText(this, "Vous avez cliqué sur l'image", Toast.LENGTH_SHORT).show()
-
+            startActivity(Intent(this, DiarraActivity::class.java))
         }
     }
 
-    private fun onClickSwitchActivity(view: Any, function: () -> Unit): Any {
+    //TODO You dont need to use this method down below
+    //TODO The switch will be handled inside the DiarraActivity
+    /*private fun onClickSwitchActivity(view: Any, function: () -> Unit): Any {
         intent = onNewIntent(this, DiarraActivity.class));
         startActivity (intent);
         return
 
-    }
+    }*/
 
 
 
